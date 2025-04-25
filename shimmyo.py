3599e866a04e5eb629d28b44a19de1212a8880fd
shimmyo <a21tl018@st.osaka-cu.ac.jp>
2025-04-25 22:25:11 +0900

    a

--- shimmyo.py
+++ shimmyo.py
@@ -41,7 +41,7 @@ def get_image():
     prompt = "car"
 
     response = requests.post(
-        "https://api.stability.ai/v2beta/stable-image/generate/ultra",
+        "https://api.stability.ai/v2beta/stable-image/generate/sdxl-1.0",
         headers={
             "authorization": f"Bearer {API_KEY}",
             "accept": "image/*"
